aa0a18da30982609594aa288f18d7583c9b54815
fasatrix <fasatrix@gmail.com>
2016-08-28 12:57:24 +1200

    second commit

--- compare.py
+++ compare.py
@@ -1,20 +1,21 @@
 import csv
+def lineNumber(file1, file2):
+	if len(list(readera)) == len(list(readerb)):
+		return True
+	else:
+		return False
+
 with open('filea.csv', 'rU') as filea,  open('fileb.csv', 'rU') as fileb:
 	readera = csv.DictReader(filea)
 	readerb = csv.DictReader(fileb)
 	count = 0
-	def lineNumber(file1, file2):
-		if len(list(readera)) == len(list(readerb)):
-			return True
-		else:
-			return False
-	if lineNumber:
-		for rowa,rowb in zip(readera, readerb):
+	for rowa,rowb in zip(readera, readerb):
+		if lineNumber(filea,fileb):
 			diffkeys = [k for k in rowa if rowa[k] != rowb[k]]
 			count = count+1
 			for k in diffkeys:
 			 	print "Line:", count,"Column:", k, ':', "Expected:",rowa[k], '->', "Actual:", rowb[k] 
-	else:
-  		print "The two files have different line number. Check sources"
-filea.close()
-fileb.close()
+		else:
+	  		print "The two files have different line number. Check sources"
+	filea.close()
+	fileb.close()
